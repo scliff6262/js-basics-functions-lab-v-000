4b7df38891f9066b243d53bc04cdc77c0ac8f137
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 16:42:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+  let answer;
+  if (street >= 42){
+    answer = street - 42
+  } else {
+    answer = 42 - street
+  }
+}
